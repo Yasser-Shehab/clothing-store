2d6abc0f9af713fc474aef59f0deb49ba3a9f27e
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-04-18 00:31:15 +0300

    Navbar fixed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Categories from "../components/Categories/Categories";
 import Nav from "../components/Nav/Nav";
+import Hero from "../components/Hero/Hero";
 import Title from "../components/Section-Title/Title";
 
 function Home() {
   return (
     <>
       <Nav />
+      <Hero />
       <Title />
       <Categories />
     </>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,47 +3,33 @@ import Hero from "../Hero/Hero";
 import "./style/Nav.style.css";
 
 function Nav() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  // useEffect(() => {
-  //   function watchWidth() {
-  //     if (window.innerWidth < 768) {
-  //       setIsMobile(true);
-  //     } else {
-  //       setIsMobile(false);
-  //     }
-  //   }
-  //   window.addEventListener("resize", watchWidth);
-
-  //   return function () {
-  //     window.removeEventListener("resize", watchWidth);
-  //   };
-  // }, []);
-
-  function toggle() {
-    setIsMobile((prev) => !prev);
-  }
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const toggle = () => {
+    setMobileMenuOpen((prev) => !prev);
+  };
+  // onClick={toggle} className={isMobile ? "menu-btn open" : "menu-btn"}
 
   return (
-    <div className="nav-container">
-      <nav className="navbar container">
-        <div className="nav-logo">
-          <img src={process.env.PUBLIC_URL + "assets/images/logo.svg"} />
-        </div>
-        <div onClick={toggle} className={isMobile ? "menu-btn open" : "menu-btn"}>
-          <div className="menu-btn__burger"></div>
-        </div>
-        <div className={isMobile ? "nav-links mobile" : "nav-links"}>
-          <li>SHOP</li>
-          <li>CONTACT</li>
-          <li>SIGN IN</li>
-          <li>
-            <img className="nav__cart" src={process.env.PUBLIC_URL + "assets/images/bag.svg"} />
-          </li>
+    <>
+      <nav className="nav">
+        <div className="navbar-container">
+          <div className="nav-logo">
+            <img className="logo" src={process.env.PUBLIC_URL + "assets/images/logo.svg"} />
+          </div>
+          <div className="menu-btn" onClick={toggle}>
+            <div className={mobileMenuOpen ? "menu-btn__burger cross" : "menu-btn__burger"}></div>
+          </div>
+          <div className={mobileMenuOpen ? "nav-links-wrapper open" : "nav-links-wrapper"}>
+            <li>SHOP</li>
+            <li>CONTACT</li>
+            <li>SIGN IN</li>
+            <li>
+              <img className="nav__cart" src={process.env.PUBLIC_URL + "assets/images/bag.svg"} />
+            </li>
+          </div>
         </div>
       </nav>
-      <Hero />
-    </div>
+    </>
   );
 }
 
