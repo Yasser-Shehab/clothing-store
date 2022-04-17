8f2ab7e2f5886b89adf733fe7b39104f915b9ecd
AquaApple <yassershehab9@gmail.com>
2022-04-17 11:16:45 +0200

    Added Section Title

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
+import Title from "../components/Section-Title/Title";
 
 function Home() {
   return (
     <>
       <Nav />
+      <Title />
     </>
   );
 }
---
+++ src/components/Section-Title/Title.jsx
@@ -0,0 +1,13 @@
+import "./style/Title.style.css";
+function Title() {
+  return (
+    <>
+      <div className="title-block">
+        <h1>Categories</h1>
+        <p>SELECT A CATEGORY TO START BROWSING</p>
+      </div>
+    </>
+  );
+}
+
+export default Title;
